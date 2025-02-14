c62145771d131aece502ed5db72cf5288e141299
formy <formy03081999@gmail.com>
2025-02-14 06:44:35 -0500

    token de validación y permisos para administrador

--- routes/areaRoutes/delete_area.ts
+++ routes/areaRoutes/delete_area.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import deleteAreaValidator from '../../middleware/areaMiddleware/deleteAreaValidator';
 import deleteAreaController from "../../controllers/areaControllers/delete-area-controller";
+import verifyToken from "../../middleware/verifyToken";
+import checkRoleAndPermission from "../../middleware/checkRoleAndPermission";
 
 
 const router = express.Router();
 
-router.delete('/:id_zona_de_trabajo', deleteAreaValidator.validatorParams, deleteAreaValidator.validator, deleteAreaController);
+router.delete('/:id_zona_de_trabajo', verifyToken, checkRoleAndPermission(["ADMINISTRADOR"]), deleteAreaValidator.validatorParams, deleteAreaValidator.validator, deleteAreaController);
 
 export default router;
--- routes/clientRoutes/update_client.ts
+++ routes/clientRoutes/update_client.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import updateClientValidator from '../../middleware/clientMiddleware/updateClientValidator';
 import updateClientController from '../../controllers/clientControllers/update-client-controller';
+import checkRoleAndPermission from '../../middleware/checkRoleAndPermission';
+import verifyToken from '../../middleware/verifyToken';
 
 
 const router = express.Router();
 
-router.put('/:id_cliente',updateClientValidator.validatorParams, updateClientValidator.validator , updateClientController);
+router.put('/:id_cliente', verifyToken, checkRoleAndPermission(["ADMINISTRADOR"]), updateClientValidator.validatorParams, updateClientValidator.validator , updateClientController);
 
 export default router;
