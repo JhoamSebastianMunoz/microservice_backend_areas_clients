d4ff00b3a01ff758de6b77bf8b24bc96ad1ddddf
formy <formy03081999@gmail.com>
2025-02-15 14:38:04 -0500

    Permisos de roles faltantes

--- routes/areaRoutes/get_area.ts
+++ routes/areaRoutes/get_area.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import getAreaValidator from '../../middleware/areaMiddleware/getAreaValidator';
 import getAreaController from '../../controllers/areaControllers/get-area-controller';
+import verifyToken from '../../middleware/verifyToken';
+import checkRoleAndPermission from '../../middleware/checkRoleAndPermission';
 
 
 
 const router = express.Router();
 
-router.get('/:id_zona_de_trabajo', getAreaValidator.validatorParams, getAreaValidator.validator, getAreaController);
+router.get('/:id_zona_de_trabajo', verifyToken, checkRoleAndPermission(["ADMINISTRADOR"]), getAreaValidator.validatorParams, getAreaValidator.validator, getAreaController);
 
 
 export default router;
--- routes/clientRoutes/get_client.ts
+++ routes/clientRoutes/get_client.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import getClientValidator from '../../middleware/clientMiddleware/getClientValidator';
 import getClientController from '../../controllers/clientControllers/get-client-controller';
 import verifyToken from '../../middleware/verifyToken';
+import checkRoleAndPermission from '../../middleware/checkRoleAndPermission';
 
 
 
 const router = express.Router();
 
-router.get('/:id_cliente', getClientValidator.validatorParams, getClientValidator.validator, getClientController);
+router.get('/:id_cliente', verifyToken, checkRoleAndPermission(["ADMINISTRADOR"]), getClientValidator.validatorParams, getClientValidator.validator, getClientController);
 
 
 export default router;
